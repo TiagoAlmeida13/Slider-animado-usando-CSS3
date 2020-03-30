3b3460af584fd89f0b1767ba037a0d17429ece9a
TiagoAlmeida13 <tyygo@live.com>
2020-03-30 16:12:03 -0300

    lista de pokémons

---
+++ controller/buscarPokemon.js
@@ -0,0 +1,62 @@
+// API endpoint --------------------------------------------
+const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
+
+// Get Elements --------------------------------------------
+const searchInput = getElement('.search-input'),
+      searchButton = getElement('.search-button'),
+      container = getElement('.pokemon'),
+      erroMessage = getElement('.error');
+
+var pokeName, // Nome ou numero passado na caixa de busca
+    pokemon, // Responsavel por guardar os dados recebidos da API
+    card; // Responsavel por receber o HTML 
+
+// Build Functions --------------------------------------------
+
+// Função para reduzir a escrita na captura de elementos HTML
+function getElement(element) {
+  return document.querySelector(element);
+}
+
+// Função responsavel por fazer requisições para a API e inserir as respostas na variavel pokemon
+function requestPokeInfo(url, name) {
+  fetch(url + name)
+    .then(response => response.json())
+    .then(data => {
+      pokemon = data;
+    })
+    .catch(err => console.log(err));
+}
+
+// Função responsavel por montar o HTML exibido na pagina
+function createCard () {
+  card = `
+    <div class="pokemon-picture">
+      <img src="${pokemon.sprites.front_default}" alt="Sprite of ${pokemon.name}">
+    </div>
+    <div class="pokemon-info">
+        <h1 class="name">Nome: ${pokemon.name}</h1>
+        <h3 class="number">Nº ${pokemon.id}</h3>
+        <h3 class="type">Tipo: ${pokemon.types.map(item => item.type.name).toString()}</h3>
+        <h3 class="skill">Habilidades: ${pokemon.moves.map(item => ' ' + item.move.name).toString()}</h3>
+        <h3 class="weight">Peso: ${pokemon.weight  / 10}kg</h3>
+        <h3 class="height">Altura: ${pokemon.height  / 10}m</h3>
+    </div>`;
+  return card;
+}
+
+// Função que faz a chamada das principais funções e inicia o app
+function startApp(pokeName) {
+  requestPokeInfo(baseUrl, pokeName);
+
+  setTimeout(function () {
+      container.innerHTML = createCard();
+  }, 2000);
+}
+
+// Add Events --------------------------------------------
+searchButton.addEventListener('click', event => {
+  event.preventDefault();
+  pokeName = searchInput.value.toLowerCase();
+  startApp(pokeName);
+});
